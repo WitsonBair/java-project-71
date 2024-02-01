2f0bf033a6f96f4798fb886966f4618987ba7003
Bayar Khyolgo <tsyden2.bair@gmail.com>
2024-02-01 19:16:06 +0800

    0.2.0 | Gradle config
    Опять этот build.gradle.kts. Хекслет советует Kotlin, и сам Gradle
    советует Kotlin, но там куча ошибок и то, что даёт Хекслет не работает и
    приходиться самому искать в интернете как именно надо добавить новую
    конфигу.
    Обычные терминальные команды кажустся проще. Единственное бы только
    разобраться бы как в Makefile сделать так, чтобы за одну make команду
    выполнялось несколько терминальных. В прошлый раз пробовал, но особо
    ничего не вышло. как в Makefile сделать так, чтобы за одну make команду
    выполнялось несколько терминальных. В прошлый раз пробовал, но особо
    ничего не вышло.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
-    id("java")
+    jacoco
+    application
+    checkstyle
+    distribution
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
+
+application {mainClass.set("hexlet.code.App")}
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
@@ -16,4 +23,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val run by tasks.getting(JavaExec::class) {
+    standardInput = System.`in`
 }
